2d1384aecb36ba322bcd138134723e22c9259c33
yung1231 <pcymichael@gmail.com>
2023-04-04 13:55:53 +0800

    [Update] dir variable & display the number of files proces

--- run.bat
+++ run.bat
@@ -1,25 +1,34 @@
 @echo off
-setlocal
+setlocal EnableDelayedExpansion
 
 echo --- Start IDAPython ---
+echo.
 
 set ida_path=D:\IDA_Pro_7.5\idat64.exe
 set script_path=D:\idapython\opcode_extract.py
 set target_dir=D:\dummy
 set save_dir=D:\save_dir
 
-if not exist "%benign_dir%" (
-    mkdir "%benign_dir%"
-    echo Create %benign_dir% dir
+if not exist "%save_dir%" (
+    mkdir "%save_dir%"
+    echo [+] Create %save_dir% dir
 ) else (
-    echo %benign_dir% directory already exists.
+    echo [+] %save_dir% directory already exists.
 )
+
+echo [+] Start analysis.
+set count=0
 
 for %%f in ("%target_dir%\*") do (
-    %ida_path% -A -S"%script_path% %%~nxf %benign_dir%" "%%~ff"
+    %ida_path% -A -S"%script_path% %%~nxf %save_dir%" "%%~ff"
     del "%%~dpnf.i64"
+    set /a count+=1
+    echo Processed !count! files.
 )
 
+echo.
 echo --- End IDAPython ---
+
+pause
 
 endlocal

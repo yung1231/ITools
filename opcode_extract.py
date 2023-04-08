d7abe6838d4a82d71715b1ad4d9390489e73421d
yung1231 <pcymichael@gmail.com>
2023-04-08 23:25:07 +0800

    [Update] Remove print

--- opcode_extract.py
+++ opcode_extract.py
@@ -8,28 +8,21 @@ import sys
 
 idaapi.auto_wait()
 
-print('--- Start IDAPython ---')
-
-
 opcodes = list()
 for function_ea, real_name in idautils.Names():
-    print('real_name: ', real_name, '\tfunction_ea: ', function_ea)
     opcode = ""
     for ins in idautils.FuncItems(function_ea):
-        print('ins: ', ins, hex(ins))
         if idaapi.is_code(idaapi.get_flags(ins)):
-            cmd = idc.GetDisasm(ins)
-            mnem = cmd.split(' ')[0]
+            asm = idc.GetDisasm(ins)
+            mnem = asm.split(' ')[0]
             if mnem[-1]==';':
                 opcode+=mnem[:-1]+' '
             else:
                 opcode+=mnem+' '
     if len(opcode.rstrip()) and len(opcode.rstrip())>18:
-        opcodes.append(opcode.rstrip())
-    print('-'*50)     
+        opcodes.append(opcode.rstrip())    
     
 with open(f"{idc.ARGV[2]}\{idc.ARGV[1]}.json", "a") as f:
     json.dump({'opcodes': opcodes}, f)
 
-print('--- End IDAPython ---')
 idc.qexit(0)
